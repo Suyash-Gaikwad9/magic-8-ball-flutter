9dc5950d577c372ca61488a35db7e73520dee5e0
Suyash <suyash.gaikwad21@vit.edu>
2022-10-27 16:04:22 +0530

    Decide from the Output

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,44 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.cyan,
+          appBar: AppBar(
+            title: Text('Ask Me Anything'),
+          ),
+          body: BallPage(),
+        ),
       ),
     );
+
+class BallPage extends StatefulWidget {
+  @override
+  State<BallPage> createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int ballNumber = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  ballNumber = Random().nextInt(5) + 1;
+                  print('ball number = $ballNumber');
+                });
+              },
+              child: Image.asset('images/ball$ballNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
